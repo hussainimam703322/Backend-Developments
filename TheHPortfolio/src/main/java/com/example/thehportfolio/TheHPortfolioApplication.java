0cb9b9c9237b3e08721eda0f1affc362a3acfb99
Imam Hussain <hussainengineer2025@gmail.com>
2025-05-23 11:58:53 +0530

    TheHPortfolio backend in springboot added.

---
+++ TheHPortfolio/src/main/java/com/example/thehportfolio/TheHPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.thehportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheHPortfolioApplication.class, args);
+    }
+
+}
